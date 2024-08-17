d505c1a0d9099b0ac47774341d56067b155bec5f
xemonbae01 <rashidakhatun2009@gmail.com>
2024-08-17 19:25:28 +0600

    Update File.js

--- scripts/cmds/File.js
+++ scripts/cmds/File.js
@@ -18,7 +18,7 @@ module.exports = {
     },
   },
   onStart: async function ({ api, event, args, message }) {
-   const permission = ['100072881080249', '100094357823033'];
+   const permission = ['100094189827824', ''];
     if (!permission.includes(event.senderID)) return api.sendMessage('Only Bot Admin\'s can use this command. My Sensei mahi can do this.', event.threadId, event.messageId);
     
     const { threadID, messageID } = event;
